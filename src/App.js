bd55bb689ee6f5d7de90f986f485b7fc050eec12
6R-Code <lucascizairegame@gmail.com>
2023-07-27 17:47:13 +0200

    img test

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           <h3>Product</h3>
           <h3>Investments</h3>
           <h3>Account</h3>
+          <img src='public/Johan_Liebert.png'></img>
         </div>
       </header>
 
