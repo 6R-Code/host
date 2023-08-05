ecec0ec27619151f16cb0c286fd31e41f51d98f1
6R-Code <lucascizairegame@gmail.com>
2023-08-05 18:51:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <h3>Account</h3>
           <h3>Product</h3>
           <h3>Investments</h3>
-          <img src='host/src/image.png' alt='Image'></img>
+          <img src='src/image.png' alt='Image'></img>
         </div>
       </header>
 
