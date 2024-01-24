65a37a96738434af567f37e6ef2bb7c65ffaa6ce
6R-Code <lucascizairegame@gmail.com>
2024-01-24 19:21:38 +0100

    Update Scene.jsx

--- src/component/Scene.jsx
+++ src/component/Scene.jsx
@@ -15,7 +15,7 @@ export default function Model(props) {
   return (
     <group {...props} dispose={null}>
       <group position={[0, 0, 0]} rotation={[-Math.PI, 0, 0]}>
-        <group position={[-19.003, -15.152, -1.409]}>
+        <group position={[0, 0, 0]}>
           <mesh geometry={nodes.Object_2.geometry} material={materials.Material_0} />
           <mesh geometry={nodes.Object_3.geometry} material={materials.Material_0} />
           <mesh geometry={nodes.Object_4.geometry} material={materials.Material_0} />
